ccb1314b22fac18a8d2025f3176387ddc057a829
Karim <konanfoi@gmail.com>
2025-02-23 20:47:31 -0500

    Task 3 - Created Inventory Class

--- cc_10.js
+++ cc_10.js
@@ -38,3 +38,22 @@ let order1 = new Order(501, prod1, 2);
 console.log(order1.getOrderDetails());
 console.log(prod1.getDetails());
 
+//Task 3 - Creating an Inventory Class
+class Inventory {
+    constructor() {
+        this.products =[];
+    }
+    addProduct(product) {
+        this.products.push(product);
+    }
+    listProducts() {
+        this.products.forEach(product => console.log(product.getDetails()));
+    }
+}
+let inventory = new Inventory();
+inventory.addProduct(prod1);
+inventory.listProducts();
+
+
+
+
